aeaa4c78c94933fd1077f353b727e7ee1df3be4b
Marcin Gizka <czarnymax112@gmail.com>
2023-08-29 12:47:10 +0200

    update date

--- src/components/Sidebar/ModifiedDate.jsx
+++ src/components/Sidebar/ModifiedDate.jsx
@@ -21,7 +21,7 @@ const ModifiedDate = (props) => {
     <div className="modified-date">
       <p>
         {state ? "Last updated: " : "Ostatnia aktualizacja: "}
-        <span>13.07.2023</span>
+        <span>29.08.2023</span>
         {daysPassed} {state ? "days ago " : "dni temu "}
       </p>
     </div>
